0962ee57f3a0596327acd665b7334b616125a7b0
Jwalavaraprasad Dhanikela - Cybage <Jwalavaraprasad.Dhanikela-Cybage@domo.com>
2016-05-13 16:51:27 +0530

    added security Initializer

---
+++ src/main/java/com/cyb/tms/init/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.cyb.tms.init;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@Order(2)
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
